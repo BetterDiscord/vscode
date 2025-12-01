3445dd9448f5c02e10b87f0c3d2b0ed7156460be
Kaan <90235641+zrodevkaan@users.noreply.github.com>
2025-11-30 21:46:29 -0600

    Whoops
    Git decided to ignore this file, I don't know why.

---
+++ src/helpers/index.ts
@@ -0,0 +1,10 @@
+import vscode from 'vscode'
+
+export async function startOpenSource(source: string) {
+  const doc = await vscode.workspace.openTextDocument({
+    content: source,
+    language: 'javascript',
+  });
+  await vscode.window.showTextDocument(doc);
+  await vscode.commands.executeCommand('editor.action.formatDocument');
+}
